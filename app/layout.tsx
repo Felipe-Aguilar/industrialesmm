7d3595ec6b132a29081be4974e40771d5d037095
Felipe Aguilar <felipe.aguilar0g@gmail.com>
2024-06-29 18:44:08 -0600

    Agregando imagen open graph

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,21 @@ import Menu from "@/components/menu/Menu";
 export const metadata: Metadata = {
   title: "Estructuras industriales MM ",
   description: "Estructuras y cubiertas industriales MM",
+  openGraph: {
+    title: "Estructuras y Cubiertas Industriales MM",
+    description: "Servicio de fabricación y mantenimiento a estructuras metálicas y montaje de estas, sirviendo como base a cualquier tipo de cubierta.",
+    url: 'https://industrialesmm.vercel.app/',
+    siteName: 'industrialesmm.com',
+    images: [
+        {
+            url: "https://industrialesmm.vercel.app/images/Logotipo2.svg",
+            width: 300,
+            height: 266,
+        },
+    ],
+    locale: 'es_MEX',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) {
